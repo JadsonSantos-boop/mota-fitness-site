1603438550cb3c67a7a9a4269a549ffd9655335f
JadsonSantos-boop <dinhojadson0@gmail.com>
2025-11-12 16:31:04 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: { extend: { colors: { motaGold: '#D4AF37' } } },
+  plugins: [],
+}
