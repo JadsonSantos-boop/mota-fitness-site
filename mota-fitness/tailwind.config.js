e6859eab47f966e0d73fa5d3178bbcb1635fedca
JadsonSantos-boop <dinhojadson0@gmail.com>
2025-11-12 17:56:56 -0300

    Add files via upload

--- mota-fitness/tailwind.config.js
+++ mota-fitness/tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: { colors: { motaGold: '#D4AF37' } } },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
